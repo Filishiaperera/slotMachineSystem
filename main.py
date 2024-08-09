97185596158c766d68bc20135e31edb6f5d4cca1
Filishiaperera <147603071+Filishiaperera@users.noreply.github.com>
2024-08-10 00:38:40 +0530

    created the get number of lines function to take the number of lines to bet on

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# to make the program dynamic adding global constant
+MAX_LINES=3# capitales coz this is a constant 
+MAX_BET=100
+MIN_BET=1
+
 def deposit():
     while True: # continusely ask the user to input a valied amount
         amount=input("How much would you like to deposit? $")
@@ -11,4 +16,26 @@ def deposit():
             print("Please enter a valied number!")
     return amount
 
-deposit()
+
+#collect the bet from the user/ how many lines they wanna bet on/then multiply their amount by number of lines
+def get_number_of_lines():# pick a number between 1 and 3
+    while True: # continusely ask the user to input a valied amount
+        lines=input("Enter the number of lines to bet on(1-"+str(MAX_LINES)+")? ") #converting the number into string
+        if lines.isdigit():
+            lines=int(lines)
+            if 1<=lines<=MAX_LINES:
+                break # breaking from the while loop and returning the amount
+            else:
+                print("Enter a valied number of lines.")
+        else:
+            print("Please enter a valied number!")
+    return lines
+# input-amount that i need to bet on each line
+
+
+def main():
+    balance=deposit()
+    lines=get_number_of_lines()
+    print(balance,lines)
+
+main()
